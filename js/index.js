b3a2f5b4f3967f457a3eadaa07bcf1e35a3e158b
那可心 <nakx@raysdata.com>
2021-07-18 01:15:53 +0800

    返回顶部

--- js/index.js
+++ js/index.js
@@ -2,10 +2,17 @@ const scroll = document.querySelector('.menu .scroll')
 const container = document.querySelector('.menu .scroll .container')
 const bar = document.querySelector('.menu .bar')
 const inner = document.querySelector('.menu .bar .inner')
+const banner = document.querySelector('.banner')
+const backTop = document.querySelector('.backTop')
 
 const getWidth = function ($el) {
   let width = window.getComputedStyle($el).width
   return parseFloat(width)
+}
+
+const getHeight = function ($el) {
+  let height = window.getComputedStyle($el).height
+  return parseFloat(height)
 }
 
 let scrollWidth = getWidth(scroll)
@@ -19,4 +26,24 @@ const getScrollLeft = function ($el) {
 
 scroll.addEventListener('scroll', function () {
   inner.style.transform = `translateX(${getScrollLeft(scroll) / containerWidth * barWidth}px)`
+}, {
+  passive: true,
+
+})
+
+let bannerHeight = getHeight(banner)
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > bannerHeight) {
+    backTop.classList.add('show')
+  }
+  else {
+    backTop.classList.remove('show')
+  }
+}, {
+  passive: true,
+})
+
+backTop.addEventListener('touchstart', function () {
+  window.scrollTo(0, 0)
 })
